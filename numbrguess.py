aabaf6b2c5c1556f897bce6cff29f0b1cef65f27
OmnibusPrime <80833153+OmnibusPrime@users.noreply.github.com>
2021-06-16 15:11:38 +0200

    Add files via upload

---
+++ numbrguess.py
@@ -0,0 +1,19 @@
+from random import randint
+zufz = randint(1, 100)
+eingabe = input ('Ihre Zahl bitte: ')
+eingabe = int(eingabe)
+if eingabe == zufz :
+        print ('Correct')
+if (zufz) > eingabe:
+    print ('zu niedrig')
+if (zufz) < eingabe:
+    print ('zu hoch')
+while eingabe != zufz :
+    eingabe = input ('noch eine Zahl bitte: ')
+    eingabe = int(eingabe)
+    if eingabe == zufz :
+        print ('Correct')
+    if (zufz) > eingabe:
+        print ('zu niedrig')
+    if (zufz) < eingabe:
+        print ('zu hoch')
